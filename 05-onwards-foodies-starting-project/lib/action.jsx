b0fd7c8e672334d1de3ed46203615590cf46e841
LMJ0900 <alswodnjswo@naver.com>
2024-11-24 00:13:50 +0900

    폼 작성 필수 삭제

--- 05-onwards-foodies-starting-project/lib/action.jsx
+++ 05-onwards-foodies-starting-project/lib/action.jsx
@@ -4,9 +4,9 @@ import { redirect } from "next/navigation"
 import { saveMeal } from "./meal"
 import { revalidatePath } from "next/cache"
 
-  function isInvaildText(text){
-    return !text || text.trim() === ''
-  }
+  // function isInvaildText(text){
+  //   return !text || text.trim() === ''
+  // }
 
 export async function shareMeal(prevState,formData) {
 
@@ -18,15 +18,15 @@ export async function shareMeal(prevState,formData) {
       creator: formData.get('name'),
       creator_email: formData.get('email'),
     }
-    if(isInvaildText(meal.title) || isInvaildText(meal.summary) ||
-    isInvaildText(meal.instruction) || isInvaildText(meal.creator) ||
-    isInvaildText(meal.creator_email) ||
-    !meal.creator_email.includes('@') ||
-    !meal.image || meal.image.size === 0){
-      return {
-        message : 'Invalid Input'
-      }
-      }
+    // if(isInvaildText(meal.title) || isInvaildText(meal.summary) ||
+    // isInvaildText(meal.instruction) || isInvaildText(meal.creator) ||
+    // isInvaildText(meal.creator_email) ||
+    // !meal.creator_email.includes('@') ||
+    // !meal.image || meal.image.size === 0){
+    //   return {
+    //     message : 'Invalid Input'
+    //   }
+    //   }
 
     await saveMeal(meal)
     revalidatePath('meals/')
